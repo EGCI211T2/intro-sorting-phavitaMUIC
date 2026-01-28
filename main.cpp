4c4a842e95a9b46fc3d070957dd381bc239704d1
phavitaMUIC <phavita.cha@student.mahidol.edu>
2026-01-28 08:56:01 +0000

    SelectionSort

--- main.cpp
+++ main.cpp
@@ -28,7 +28,7 @@ int main(int argc, char * argv[]) {
     cout<<"Before sorting:";
     display(a,N);
     
-    sort(a,N);
+    selectionsort(a,N);
     cout<<"After sorting:";
     display(a,N);
     
--- sorting.h
+++ sorting.h
@@ -1,5 +1,6 @@
 void swap(int &a,int &b);
-void sort(int *a,int N);
+void bubblesort(int *a,int N);
+void selectionsort(int *a,int N);
 void display(int *a,int N);
 
 void swap(int &a,int &b){
@@ -9,22 +10,47 @@ void swap(int &a,int &b){
     b=c;
 }
 
-void sort(int *a, int N){
+void bubblesort(int *a, int N){
     int i,j;
+    int sorted;
     for (j=0;j<N-1;j++){
+        sorted =0;
         for (i=0;i<N-1;i++){
             if (a[i]>a[i+1])
             swap(a[i],a[i+1]);
+        sorted = 1;
         }
+        if (sorted == 0) 
+        break;
+    cout<<endl;
 
     }
     
 }
+
 void display (int *a, int N){
     int i;
     for (i=0; i<N;i++){
         cout<<" "<<a[i];
     }
     cout<<endl;
+}
 
+void selectionsort(int *a,int N){
+    int i,mi,j;
+    int min=a[0];
+    mi=0;
+    for(j=0;j<N-1;j++){
+        min = a[j];
+        mi=j;
+         for(i=j+1;i<N;i++){
+            if(min>a[i]){
+                min = a[i];
+                mi=i;
+                
+            }
+
+        }
+        swap (a[j],a[mi]);
+    }
 }
